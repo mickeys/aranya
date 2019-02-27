754fdee24dddc19c1196acfc24514c6f8542f0e8
Michael Sattler <michael.sattler@creditkarma.com>
2019-02-27 14:43:54 -0800

    main entry-point

---
+++ main.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+# -------------------------------------------------------------------------------
+# The entry point for Aranya, a general-purpose JSON-crawling spider for the
+# recursive validation of 'response' data structures against a GraphQL
+# introspection SDL (schema definition language).
+#
+# Currently used to test aranya, presumably to be used as a CI/CD yoke for
+# the automated crawling from (for example) a Jenkins environment.
+# -------------------------------------------------------------------------------
+require 'json'							# JSON (JavaScript Object Notation)
+require './spider.rb'					# the JSON-crawling spider
+
+RESPONSE = './json/cars.json'.freeze	#
+
+r = JSON.parse(File.read(RESPONSE))
+
+spider(r)
