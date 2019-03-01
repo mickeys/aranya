75971b67ce537b7c53533ea9a332aa69d2544923
Michael Sattler <michael.sattler@creditkarma.com>
2019-03-01 10:55:16 -0800

    added DEBUG and VERBOSE

--- main.rb
+++ main.rb
@@ -1,37 +1,91 @@
 #!/usr/bin/ruby
 # ===============================================================================
 # The entry point for Aranya, a general-purpose JSON-crawling spider for the
-# recursive validation of 'response' data structures against a GraphQL
-# introspection SDL (schema definition language).
+# recursive validation of '$ResponseFile' data structures against a GraphQL
+# introspection $SdlFile (schema definition language).
 #
 # Currently used to test aranya, presumably to be used as a CI/CD yoke for
 # the automated crawling from (for example) a Jenkins environment.
 # ===============================================================================
 require 'json'							# JSON (JavaScript Object Notation)
 require './spider.rb'					# the JSON-crawling spider
-require 'pp'
-
-RESPONSE = './json/cars.json'.freeze	#
-#RESPONSE = './json/cc_search_response.json'.freeze	#
-SDL = './json/introspection.json'.freeze	#
 
 # -------------------------------------------------------------------------------
-# Iterate over all JSON files that need opening & assign them to local variables.
+# Constants
+# -------------------------------------------------------------------------------
+$ResponseFile = './json/cc_search_response.json'.freeze
+$SdlFile = './json/introspection.json'.freeze
+$TypeMark = '__typename'.freeze
+
+# -------------------------------------------------------------------------------
+# Loop: Parse JSON file and assign contents to local variable.
 # -------------------------------------------------------------------------------
 r = s = nil ;							# can't create local vars via eval :-(
-[['r', "#{RESPONSE}"], ['s', "#{SDL}"]].each do |json_file|
+[['r', "#{$ResponseFile}"], ['s', "#{$SdlFile}"]].each do |json_file|
 	begin
 	eval "#{json_file[0]} = JSON.parse(File.read( \"#{json_file[1]}\" ))"
 	rescue JSON::ParserError => e
-		STDERR.puts "fatal: #{e.class.name} error in '#{RESPONSE}'; quitting."
+		STDERR.puts "fatal: #{e.class.name} error in '#{$ResponseFile}'; quitting."
 		exit							# quit - not much to do if JSON is bad
 	end
 end
 
-#p r['data']['__schema']['types'].length
-depth=1
-	r.each do |pair|
-		printf( "%s (%s)\n", ' '*(depth*2), pair) # pretty-print each member
+#p s['data']['__schema']['types']['RootQuery'].to_s
+##	p JSON.parse( s['data']['__schema']['types']['RootQuery'].to_s )
+#exit
+
+# -------------------------------------------------------------------------------
+# Convert a standard GraphQL $SdlFile into a ruby hash (so we can search by typename).
+# -------------------------------------------------------------------------------
+sdl = Hash.new
+
+#puts "s.l = #{s.length} #{s['data']['__schema']['types'].length}"
+#i=0
+s['data']['__schema']['types'].each do |t|
+#	name = t['name']
+	unless t['fields'].nil?				# many entries have no fields
+		sdl[ t['name'] ] = t['fields']
 	end
 
-spider( r, s )							# validate a response against an SDL
+#	j=0
+#	unless t['fields'].nil?				# many entries have no fields
+#		t['fields'].each do |row|
+#			puts "\t#{i} #{name} #{j} %25s" % [row.to_s ]
+#			j = j+1
+#		end
+#	end
+#	i = i+1
+
+#p '--- DURING -------------------------------------'
+#	value = t['fields'].to_json
+#	puts "#{name} ===> #{value}"
+#p '--- AFTER -------------------------------------'
+#	puts "#{t['fields'].to_json}"		# serialize the entry
+###	sdl[:name] = value
+end
+
+#puts "dict len = #{sdl.length}"
+#p sdl['AppDashboard'].class
+#
+#sdl['AppDashboard'].each do |x|
+#	k=0
+#	puts x['name']
+#	x.each do |y|
+#		puts "#{k} #{y}"
+#		k=k+1
+#	end
+#end
+##exit
+
+#depth=1
+#	r.each do |pair|
+#		printf( "%s (%s)\n", ' '*(depth*2), pair) # pretty-print each member
+#	end
+
+##s['data']['__schema']['types'][0]
+#sdl.each do |x|
+#	puts "\t --> #{x}"
+#end
+
+spider( r['data'], sdl )						# validate a $ResponseFile against an $SdlFile
+#		s['data']['__schema']['types'] )
--- spider.rb
+++ spider.rb
@@ -2,8 +2,8 @@
 
 # ===============================================================================
 # The structural body of Aranya, a general-purpose JSON-crawling spider for the
-# recursive validation of 'response' data structures against a GraphQL
-# introspection SDL (schema definition language).
+# recursive validation of '$Response' data structures against a GraphQL
+# introspection $SDL (schema definition language).
 # ===============================================================================
 require 'singleton'						# dynamic dispatch done via one Validate
 
@@ -17,16 +17,16 @@ require 'singleton'						# dynamic dispatch done via one Validate
 #end
 
 # -------------------------------------------------------------------------------
-# Dispatch types found in the response object to helper functions.
+# Dispatch types found in the $Response object to helper functions.
 #
 # TO-DO: replace with a general mechanism to find and validate against a typedef
-# SDL and attempt to dispatch *optional* hand-curated type-specific test functions.
+# $SDL and attempt to dispatch *optional* hand-curated type-specific test functions.
 # -------------------------------------------------------------------------------
 class Validate
 	include Singleton					# just one central dispatcher
 
 	# ---------------------------------------------------------------------------
-	# TO-DO: general mechanism for getting a `__typedef` and gathering the SDL
+	# TO-DO: general mechanism for getting a `__typedef` and gathering the $SDL
 	# instead of defining one function for each and every...
 	# ---------------------------------------------------------------------------
 	def test
@@ -47,19 +47,50 @@ def spider( response, sdl, depth=0 )
 	v = Validate.instance				# we'd like a dispatcher, please
 
 	typename = response['__typename']
-#	typename = response['name']
-
 	unless typename.nil?				# if there's a '_typename' at this level
+		printf( "%s %s\n", ' '*(depth), typename ) if $VERBOSE
+
+		# -----------------------------------------------------------------------
+		# Dispatch if there's a typename-specific validator
+		# -----------------------------------------------------------------------
 		if v.respond_to?(typename)		# and validate knows about it
 			v.send(typename)			# then give it a go
-		else
-			STDERR.puts "error: unknown __typename '#{typename}'; skipping."
+
+#		else							# is ok not every level has a __typename
+#			STDERR.puts "error: unknown __typename '#{typename}'; skipping."
+		end
+
+		# -----------------------------------------------------------------------
+		# Make a array of all the 'name' elements at this level of the sdl.
+		#
+		# TO-DO: move this and make global hash of typename and top-level members
+		# -----------------------------------------------------------------------
+		definition = []
+		sdl[typename].each do |schema|
+			schema.each do |member|
+				definition << member[1] if member[0] == 'name'
+			end
+		end
+
+		# -----------------------------------------------------------------------
+		# Compare $Response object (what exists) against the $SDL (which contains
+		# all that is permitted).
+		# -----------------------------------------------------------------------
+		response.each do |pair|
+			printf( "%s %s\n", ' '*(depth+2), pair[0] ) if $VERBOSE
+
+			unless typename != $TypeMark || definition.include?(pair[0])
+				STDERR.puts "error: #{typename} doesn't include #{pair[0]}."
+			else
+				puts "found #{pair[0]} in #{typename}" if $DEBUG
+			end
 		end
 	end
 
+	# ---------------------------------------------------------------------------
+	# Having checked this JSON level, recurse over children of this level.
+	# ---------------------------------------------------------------------------
 	response.each do |pair|
-		# printf( "%s (%s)\n", ' '*(depth*2), pair) # pretty-print the object
-
 		if pair[1].is_a?(Hash)			# any children an object by themselves?
 			spider(pair[1], sdl, depth+1 )	# recurse on that child
 		end
