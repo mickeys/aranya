1cd684008907e2968ad6178f1e2012a15a4772fe
Michael Sattler <michael.sattler@creditkarma.com>
2019-06-17 14:56:49 -0700

    properly do arrays

--- spider.rb
+++ spider.rb
@@ -6,6 +6,7 @@
 # introspection $SDL (schema definition language).
 # ===============================================================================
 require 'singleton'						# dynamic dispatch done via one Validate
+require 'pry-byebug'
 
 # -------------------------------------------------------------------------------
 # I used to handle NoMethodError exceptions when dispatch found an unknown type,
@@ -26,8 +27,6 @@ class Validate
 	include Singleton					# just one central dispatcher
 
 	# ---------------------------------------------------------------------------
-	# TO-DO: general mechanism for getting a `__typedef` and gathering the $SDL
-	# instead of defining one function for each and every...
 	# ---------------------------------------------------------------------------
 	def test
 		puts 'TEST'
@@ -44,11 +43,17 @@ end
 
 # -------------------------------------------------------------------------------
 def spider( response, sdl, depth=0 )
+
+#exit if depth > 3
+
 	v = Validate.instance				# we'd like a dispatcher, please
 
-	typename = response['__typename']
+	printf( "%s%s\n", ' '*(depth*2), 'Response keys:' ) if $VERBOSE
+	printf( "%s%s\n", ' '*(depth*2), response.keys[0] ) if $VERBOSE
+
+	typename = response[ $TypeMark ]
+=begin
 	unless typename.nil?				# if there's a '_typename' at this level
-		printf( "%s %s\n", ' '*(depth), typename ) if $VERBOSE
 
 		# -----------------------------------------------------------------------
 		# Dispatch if there's a typename-specific validator
@@ -56,8 +61,8 @@ def spider( response, sdl, depth=0 )
 		if v.respond_to?(typename)		# and validate knows about it
 			v.send(typename)			# then give it a go
 
-#		else							# is ok not every level has a __typename
-#			STDERR.puts "error: unknown __typename '#{typename}'; skipping."
+#		else							# is ok, not every level has a typename
+#			STDERR.puts "error: unknown #{$TypeMark} '#{typename}'; skipping."
 		end
 
 		# -----------------------------------------------------------------------
@@ -73,26 +78,43 @@ def spider( response, sdl, depth=0 )
 		end
 
 		# -----------------------------------------------------------------------
-		# Compare $Response object (what exists) against the $SDL (which contains
+		# Compare response object (what exists) against the SDL (which contains
 		# all that is permitted).
 		# -----------------------------------------------------------------------
 		response.each do |pair|
-			printf( "%s %s\n", ' '*(depth+2), pair[0] ) if $VERBOSE
-
-			unless typename != $TypeMark || definition.include?(pair[0])
+			printf( "%s%s\n", ' '*((depth*2)+2), pair[0] ) if $VERBOSE
+			unless typename != $TypeMark || definition.include?( pair[0] )
 				STDERR.puts "error: #{typename} doesn't include #{pair[0]}."
 			else
 				puts "found #{pair[0]} in #{typename}" if $DEBUG
 			end
 		end
 	end
-
+=end
 	# ---------------------------------------------------------------------------
 	# Having checked this JSON level, recurse over children of this level.
 	# ---------------------------------------------------------------------------
 	response.each do |pair|
+#binding.pry
+		printf( "%s%d checking %s\n", ' '*((depth*2)+2), depth, pair[1].to_s[0,80] ) if $VERBOSE
+
 		if pair[1].is_a?(Hash)			# any children an object by themselves?
-			spider(pair[1], sdl, depth+1 )	# recurse on that child
+
+			printf( "%s%d 1_would_spider %s %s %s\n", ' '*((depth*2)+2), depth, pair[1].class, pair[0], pair[1].to_s[0,80] ) if $VERBOSE
+			spider( pair[1], sdl, depth+1 )	# recurse on that child
+
+		elsif pair[1].is_a?(Array)		# an array of somethings? iterate!
+
+			puts 'found an array'
+			pair[1].each do |thing|
+#				printf( "%s%d would_spider %s %s %s\n", ' '*((depth*2)+2), depth, pair[1].class, pair[0], thing.to_s[0,80] ) if $VERBOSE
+				printf( "%s%d 2_would_spider %s %s %s\n", ' '*((depth*2)+2), depth, thing.class, thing.to_s[0,80], thing.to_s[0,80] ) if $VERBOSE
+
+				spider( thing, sdl, depth+1 )	# recurse on that child
+			end
+
+		else
+			printf( "%s%d rejecting %s %s %s\n", ' '*((depth*2)+2), depth, pair[1].class, pair[0], pair[1].to_s[0,80] ) if $VERBOSE
 		end
 	end
 end
