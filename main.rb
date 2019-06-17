8a367cb02a789834546cd3fb02d465240d3a8b16
Michael Sattler <michael.sattler@creditkarma.com>
2019-06-17 14:57:13 -0700

    backtrace; etc.

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 # ===============================================================================
 # The entry point for Aranya, a general-purpose JSON-crawling spider for the
 # recursive validation of '$ResponseFile' data structures against a GraphQL
@@ -8,84 +8,46 @@
 # the automated crawling from (for example) a Jenkins environment.
 # ===============================================================================
 require 'json'							# JSON (JavaScript Object Notation)
+require 'pry-byebug'
 require './spider.rb'					# the JSON-crawling spider
+
+$VERBOSE=true # deeper in-program debugging
 
 # -------------------------------------------------------------------------------
 # Constants
 # -------------------------------------------------------------------------------
 $ResponseFile = './json/cc_search_response.json'.freeze
-$SdlFile = './json/introspection.json'.freeze
+$SdlFile = './json/ck_introspection.json'.freeze
+
 $TypeMark = '__typename'.freeze
 
 # -------------------------------------------------------------------------------
-# Loop: Parse JSON file and assign contents to local variable.
+# Loop: Parse JSON file(s) and assign contents to local variable.
 # -------------------------------------------------------------------------------
-r = s = nil ;							# can't create local vars via eval :-(
-[['r', "#{$ResponseFile}"], ['s', "#{$SdlFile}"]].each do |json_file|
+r = s = nil ;							# can't create local vars via eval :-/
+
+[	[ 'r', "#{$ResponseFile}" ],		# that which was returned to us
+	[ 's', "#{$SdlFile}" ]				# the source of truth
+].each do |json_file|
+	puts "#{json_file[1]}" if $VERBOSE
 	begin
-	eval "#{json_file[0]} = JSON.parse(File.read( \"#{json_file[1]}\" ))"
-	rescue JSON::ParserError => e
+		eval "#{json_file[0]} = JSON.parse(File.read( \"#{json_file[1]}\" ))"
+	rescue JSON::ParserError => e		# do if things went bad quickly
 		STDERR.puts "fatal: #{e.class.name} error in '#{$ResponseFile}'; quitting."
+		puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
 		exit							# quit - not much to do if JSON is bad
 	end
 end
-
-#p s['data']['__schema']['types']['RootQuery'].to_s
-##	p JSON.parse( s['data']['__schema']['types']['RootQuery'].to_s )
-#exit
 
 # -------------------------------------------------------------------------------
 # Convert a standard GraphQL $SdlFile into a ruby hash (so we can search by typename).
 # -------------------------------------------------------------------------------
 sdl = Hash.new
 
-#puts "s.l = #{s.length} #{s['data']['__schema']['types'].length}"
-#i=0
 s['data']['__schema']['types'].each do |t|
-#	name = t['name']
 	unless t['fields'].nil?				# many entries have no fields
-		sdl[ t['name'] ] = t['fields']
+		sdl[ t['name'] ] = t['fields']	# capture fields for the ones that do
 	end
-
-#	j=0
-#	unless t['fields'].nil?				# many entries have no fields
-#		t['fields'].each do |row|
-#			puts "\t#{i} #{name} #{j} %25s" % [row.to_s ]
-#			j = j+1
-#		end
-#	end
-#	i = i+1
-
-#p '--- DURING -------------------------------------'
-#	value = t['fields'].to_json
-#	puts "#{name} ===> #{value}"
-#p '--- AFTER -------------------------------------'
-#	puts "#{t['fields'].to_json}"		# serialize the entry
-###	sdl[:name] = value
 end
 
-#puts "dict len = #{sdl.length}"
-#p sdl['AppDashboard'].class
-#
-#sdl['AppDashboard'].each do |x|
-#	k=0
-#	puts x['name']
-#	x.each do |y|
-#		puts "#{k} #{y}"
-#		k=k+1
-#	end
-#end
-##exit
-
-#depth=1
-#	r.each do |pair|
-#		printf( "%s (%s)\n", ' '*(depth*2), pair) # pretty-print each member
-#	end
-
-##s['data']['__schema']['types'][0]
-#sdl.each do |x|
-#	puts "\t --> #{x}"
-#end
-
-spider( r['data'], sdl )						# validate a $ResponseFile against an $SdlFile
-#		s['data']['__schema']['types'] )
+spider( r['data'], sdl )				# validate a response against an SDL
